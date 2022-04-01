565b24e5a73bb5b4d7727b0612569937a327a319
blizpear <blizpear@gmail.com>
2022-04-01 18:54:12 +0700

    Создал анимации

---
+++ component/ui/src/main/java/com/sequenia/component/ui/animation/VisabilityExtentions.kt
@@ -0,0 +1,42 @@
+package com.sequenia.component.ui.animation
+
+import android.view.View
+import androidx.core.view.isVisible
+
+private const val ALPHA_VISIBLE = 1F
+private const val ALPHA_INVISIBLE = 0F
+private const val ALPHA_DURATION = 100L
+
+fun View.showWithFade() {
+	animate().cancel()
+
+	if (isVisible && alpha == ALPHA_VISIBLE) {
+		return
+	}
+	if (alpha == ALPHA_VISIBLE){
+		alpha = ALPHA_INVISIBLE
+	}
+
+	animate()
+		.alpha(ALPHA_VISIBLE)
+		.setDuration(ALPHA_DURATION)
+		.withStartAction { isVisible = true }
+		.start()
+}
+
+fun View.hideWithFade() {
+	animate().cancel()
+
+	if (!isVisible) {
+		return
+	}
+
+	animate()
+		.alpha(ALPHA_INVISIBLE)
+		.setDuration(ALPHA_DURATION)
+		.withEndAction {
+			isVisible = false
+			alpha = ALPHA_VISIBLE
+		}
+		.start()
+}
